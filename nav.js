6297f1df0fa7d2e4fe7eada87ea0dd50172db161
mathematics-software <49321435+mathematics-software@users.noreply.github.com>
2019-09-23 10:35:34 -0500

    Update nav.js

--- nav.js
+++ nav.js
@@ -5,3 +5,24 @@ document.getElementById("home").addEventListener("mouseup",function(e){
 document.getElementById("new").addEventListener("mouseup",function(e){
   window.location.reload();
 });
+
+function showBorder(elem){
+  elem.style.border = '5px outset darkgray';
+}
+
+function directTo(elem,page){
+  elem.style.border = '5px solid black';
+  document.location.href='https://mathematics-software.github.io/Solitaire/' + page;
+}
+
+try{
+  var classic = document.getElementById("classic");
+  var nerts = document.getElementById("nerts");
+  var pyramid = document.getElementById("pyramid");
+  classic.addEventListener("mousedown",function(e){showBorder(e.target);});
+  nerts.addEventListener("mousedown",function(e){showBorder(e.target);});
+  pyramid.addEventListener("mousedown",function(e){showBorder(e.target);});
+  classic.addEventListener("mouseup",function(e){directTo(e.target,"solitaire.html")});
+  nerts.addEventListener("mouseup",function(e){directTo(e.target,"nerts.html")});
+  pyramid.addEventListener("mouseup",function(e){directTo(e.target,"pyramid.html")});
+}catch(Exception e){}
