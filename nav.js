03a245bf9b2835d8f4fff401b16ee8ac623ef6b5
mathematics-software <49321435+mathematics-software@users.noreply.github.com>
2019-09-23 09:39:31 -0500

    Update nav.js

--- nav.js
+++ nav.js
@@ -1,1 +1,9 @@
+document.body.innerHTML += "<button id='home'><i class='fa fa-home'></i>&nbsp;Home</button><button id='new'><i class='fa fa-refresh'></i>&nbsp;New Game</button>";
 
+document.getElementById("home").addEventListener("mouseup",function(e){
+  window.location.href = "https://mathematics-software.github.io/Solitaire/index.html";
+});
+
+document.getElementById("home").addEventListener("mouseup",function(e){
+  window.location.reload();
+});
